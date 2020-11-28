fe07c5b79dec2597e79720642e2fe090fa4134d8
Kayhan-MAC <kayhan9204@gmail.com>
2020-11-28 14:32:17 +0600

    1.0.2

--- app/src/main/java/com/kayhan/sampleactivity/MainActivity.java
+++ app/src/main/java/com/kayhan/sampleactivity/MainActivity.java
@@ -1,7 +1,5 @@
 package com.kayhan.sampleactivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
